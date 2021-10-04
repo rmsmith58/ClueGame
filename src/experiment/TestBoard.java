167cbec2338d1032bdafd5b4b9329084b8ace818
Mikayla <mrsherwood@mines.edu>
2021-10-04 16:10:10 -0600

    Implemented TestBoard Class.

--- src/experiment/TestBoard.java
+++ src/experiment/TestBoard.java
@@ -1,5 +1,23 @@
 package experiment;
+import java.util.Set;
 
 public class TestBoard {
-
+	private Set<TestBoardCell> targets;
+	
+	public TestBoard() {
+		TestBoardCell tbc = new TestBoardCell(1, 1);
+	}
+	
+	public void calcTargets(TestBoardCell startCell, int pathlength) {
+		
+	}
+	
+	public Set<TestBoardCell> getTargets(){
+		return targets;
+	}
+	 public TestBoardCell getCell(int row, int col) {
+		 TestBoardCell tbc = new TestBoardCell(1, 1);
+		 return tbc;
+	 }
+	
 }
