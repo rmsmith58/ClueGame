534f15860cc743959c07fb7f1d03f5238752e977
Ryne Smith <rmsmith@mines.edu>
2021-10-04 15:53:29 -0600

    Implemented method stubs for TestBoardCell as outlined on project writeup

--- src/experiment/TestBoardCell.java
+++ src/experiment/TestBoardCell.java
@@ -1,5 +1,40 @@
 package experiment;
 
-public class TestBoardCell {
+import java.util.Set;
 
+public class TestBoardCell {
+	private Boolean isInRoom;
+	private Boolean isOccupied;
+	private Set<TestBoardCell> adjList;
+	
+	public TestBoardCell(int row, int col) {
+		//TODO do something
+	}
+	
+	public void addAdjacency(TestBoardCell cell) {
+		//TODO do something
+	}
+
+	public Boolean getRoom() {
+		return isInRoom;
+	}
+
+	public void setRoom(Boolean isInRoom) {
+		this.isInRoom = isInRoom;
+	}
+
+	public Boolean getOccupied() {
+		return isOccupied;
+	}
+
+	public void setOccupied(Boolean isOccupied) {
+		this.isOccupied = isOccupied;
+	}
+	
+	public Set<TestBoardCell> getAdjList(){
+		return this.adjList;
+	}
+	
+	
+	
 }
