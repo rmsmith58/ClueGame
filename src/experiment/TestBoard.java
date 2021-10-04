ccef6c72ac484488dbeab702e4bd5181914d3ef4
Ryne Smith <rmsmith@mines.edu>
2021-10-04 16:20:50 -0600

    implemented TestBoard constructor to create a board of TestBoardCell of a specified size in 2 dimensions

--- src/experiment/TestBoard.java
+++ src/experiment/TestBoard.java
@@ -1,11 +1,18 @@
 package experiment;
+
 import java.util.Set;
 
 public class TestBoard {
 	private Set<TestBoardCell> targets;
+	private TestBoardCell[][] board;
 	
-	public TestBoard() {
-		TestBoardCell tbc = new TestBoardCell(1, 1);
+	public TestBoard(int rows, int cols) {
+		board = new TestBoardCell[rows][cols];
+		for(int i = 0; i < rows; i++) {
+			for(int j = 0; j < cols; j++) {
+				board[i][j] = new TestBoardCell(i, j);
+			}
+		}
 	}
 	
 	public void calcTargets(TestBoardCell startCell, int pathlength) {
--- src/tests/BoardTestsExp.java
+++ src/tests/BoardTestsExp.java
@@ -2,10 +2,16 @@ package tests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class BoardTestsExp {
+import experiment.*;
 
+class BoardTestsExp {
+	@BeforeEach
+	void setup() {
+		TestBoard board = new TestBoard();
+	}
 	@Test
 	void test() {
 		fail("Not yet implemented");
