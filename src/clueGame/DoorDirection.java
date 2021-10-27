96249bc1081b057121b154d494d9136951b26f57
Mikayla <mrsherwood@mines.edu>
2021-10-27 15:14:32 -0600

    Reduced the size of DoorDirection because we weren't using the toString method within the enum type and got rid of that unused code.

--- src/clueGame/DoorDirection.java
+++ src/clueGame/DoorDirection.java
@@ -1,13 +1,6 @@
 package clueGame;
 
 public enum DoorDirection {
-	//These are the options for our enum type, not sure whether they would take in a string or not
-	//but still accounted for it.
-	UP("Up"), DOWN("Down"), LEFT("Left"), RIGHT("Right"), NONE("None");
-	private String direction;
-	
-	//This would use the direction the user wants to go and then interpret which enum type to use.
-	DoorDirection(String value){
-		direction = value;
-	}
+	//These are the options for our enum type.
+	UP, DOWN, LEFT, RIGHT, NONE;
 }
