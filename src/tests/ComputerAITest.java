2445e63114caadd894056aefa73bd587d213ad33
Mikayla <mrsherwood@mines.edu>
2021-11-04 17:28:47 -0600

    Finished the all the tests in ComputerAITest Class. I think?

--- src/tests/ComputerAITest.java
+++ src/tests/ComputerAITest.java
@@ -21,6 +21,7 @@ import clueGame.Board;
 import clueGame.BoardCell;
 import clueGame.Card;
 import clueGame.CardType;
+import clueGame.ComputerPlayer;
 import clueGame.Player;
 import clueGame.Solution;
 
@@ -39,25 +40,13 @@ public class ComputerAITest {
 		board.setConfigFiles("BoardLayout.csv", "ClueSetup.txt");		
 		// Initialize will load config files 
 		board.initialize();
-		
-		/* Not sure if I will need this. I really need to read first.
-		// Creates the soltuion deck
+
+		// Creates the test solution deck
 		Solution test = new Solution();
 		Card room = new Card("Bathroom", CardType.ROOM);
 		Card person = new Card("Phil Hopkins", CardType.PERSON);
 		Card weapon = new Card("Cyanide", CardType.WEAPON);
 		test.setSolution(person, room, weapon);
-		
-		//Creates test card in the AI hand.
-		//Set AI computer to Dale Jenkins.
-		
-		Card judyCard = new Card("Judy Edwards", CardType.PERSON);
-		Card hallCard = new Card("Hall", CardType.ROOM);
-		Card axeCard = new Card("Axe", CardType.WEAPON);
-		board.getPlayers().
-		*/
-		//Creates test card in the Players hand.
-		
 	}
 
 	@AfterEach
@@ -65,17 +54,97 @@ public class ComputerAITest {
 		//We reset the board to not add on additional Players or Cards.
 		board.resetBoard();
 	}
-	
+
 	//Test the logic within selectTarget in the computer class.
+	//Having trouble on how to implement test when things are random.
 	@Test
 	public void selectTargets() {
-		
-		
+		//test when the player is on a walkway
+		ComputerPlayer player = new ComputerPlayer("AI", Color.red, 11, 16);
+		Set<BoardCell> targets = null;
+		for(int i = 0; i < 100; i++) {
+			targets.add(player.selectTargets());
+		}
+
+		assertTrue(targets.contains(board.getCell(10, 16)));
+		assertTrue(targets.contains(board.getCell(11, 15)));
+		assertTrue(targets.contains(board.getCell(11, 17)));
+
+		//test when he player is near a door to a room in their seen list.
+		player.setLocation(8, 5);
+		Card testRoom = new Card("Office", CardType.ROOM);
+		player.updateHand(testRoom);
+
+		for(int i = 0; i < 100; i++) {
+			targets.add(player.selectTargets());
+		}
+
+		assertTrue(targets.contains(board.getCell(7, 5)));
+		assertTrue(targets.contains(board.getCell(8, 6)));
+		assertFalse(targets.contains(board.getCell(11, 2)));
 	}
-	
+
 	//Tests the logic on how a computer creates the suggestion.
 	@Test
 	public void createSuggestion() {
+		//Checks the suggestions in the Game Room
+		ComputerPlayer player = new ComputerPlayer("AI", Color.red, 2, 2); 
+		ArrayList<Card> unseen = board.getDeck();
+		unseen.removeAll(player.getHand());
 		
+		//Holds all the suggestions created and doesn't duplicate.
+		Set<Solution> suggestions;
+
+		//randomly generates suggestions.
+		for(int i = 0; i < 100; i++) {
+			suggestions.add(player.createSuggestion());	
+		}
+
+		//Only one room card the player can only make a suggestion in the room they are in.
+		Card room = new Card("Game Room", CardType.ROOM);
+		Card person1 = new Card("Lisa Johnson", CardType.PERSON);
+		Card person2 = new Card("Judy Edwards", CardType.PERSON);
+		Card weapon1 = new Card("Axe", CardType.WEAPON);
+		Card weapon2 = new Card("Cyanide", CardType.PERSON);
+		
+		//Creates random solutions from the created card.
+		Solution sol1 = new Solution(person1, weapon1, room);
+		Solution sol2= new Solution(person1, weapon2, room);
+		Solution sol3= new Solution(person2, weapon1, room);
+		Solution sol4= new Solution(person2, weapon2, room);
+
+		//Checks to see if these random suggestions were made.
+		assertTrue(suggestions.contains(sol1));
+		assertTrue(suggestions.contains(sol2));
+		assertTrue(suggestions.contains(sol3));
+		assertTrue(suggestions.contains(sol4));
+
+		//Checks the suggestions in the Lounge
+		player.setLocation(2, 10); 
+		unseen = board.getDeck();
+		unseen.removeAll(player.getHand());
+		
+		//Clears to get rid of old suggestions.
+		suggestions.clear();
+
+		//randomly generates suggestions.
+		for(int i = 0; i < 100; i++) {
+			suggestions.add(player.createSuggestion());	
+		}
+
+		//Only one room card the player can only make a suggestion in the room they are in.
+		Card room2 = new Card("Lounge", CardType.ROOM);
+		
+		//Creates random solutions from the created card.
+		Solution solt1 = new Solution(person1, weapon1, room2);
+		Solution solt2= new Solution(person1, weapon2, room2);
+		Solution solt3= new Solution(person2, weapon1, room2);
+		Solution solt4= new Solution(person2, weapon2, room2);
+
+		//Checks to see if these random suggestions were made.
+		assertTrue(suggestions.contains(solt1));
+		assertTrue(suggestions.contains(solt2));
+		assertTrue(suggestions.contains(solt3));
+		assertTrue(suggestions.contains(solt4));
 	}
 }
