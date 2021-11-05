610d8ae20ec5bc40397d24bfd18b6443078719f2
Mikayla <mrsherwood@mines.edu>
2021-11-04 22:46:08 -0600

    Started to build some of the logic within ComputerPlayer class. Struggling on how to gain access of board variables.

--- src/clueGame/ComputerPlayer.java
+++ src/clueGame/ComputerPlayer.java
@@ -21,33 +21,47 @@ public class ComputerPlayer extends Player {
 	public Solution createSuggestion() {
 		//If the player is in a room in need to create a suggestion.
 		//The weapon and person is randomly chosen out of the unseen list, and the room is the room the player is in.
-		BoardCell location = new BoardCell(row, column);
-		Card room = new Card("Lounge", CardType.ROOM);
-		Card person = new Card("You", CardType.PERSON);
-		Card weapon = new Card("Axe", CardType.WEAPON);
-		/*
-		ArrayList<Card> unseen = deck;
+		Card room = new Card("Lounge", CardType.ROOM);	//Figure out how to get the name of the room.
+		
+		//Creates a array list of an unseen deck, and then separates that deck into people and weapons.
+		ArrayList<Card> unseen = deck;  //Figure out how to access the deck;
 		unseen.removeAll(seen);
 		ArrayList<Card> people;
-		ArrayList<Card> weapon;
+		ArrayList<Card> weapons;
 		
-		for(Card card: seen) {
+		//Separates the unseen cards into people and weapons.
+		//We only want these items because a suggestion room can only be the room the player is in.
+		for(Card card: unseen) {
 			if(card.getCardType() == CardType.PERSON) {
 				people.add(card);
 			}
 			else if(card.getCardType() == CardType.WEAPON) {
-				weapon.add(card);
+				weapons.add(card);
 			}
 		}
-		*/
 		
+		//Creates a random number for picking the cards.
+		Random rand = new Random();
+		int peopleIndex = rand.nextInt(people.size());
+		int weaponIndex = rand.nextInt(weapons.size());
+		
+		//Sets the cards for the solution.
+		Card person = people.get(peopleIndex);
+		Card weapon = weapons.get(weaponIndex);
+		
+		//Creates the solution.
 		Solution sol = new Solution(person, room, weapon);
+		
 		return sol;
 	}
 	
 	
 	public BoardCell selectTargets() {
-		BoardCell test = new BoardCell(0,0);
-		return test;
+		BoardCell location = new BoardCell(row, column);
+		if(location.isRoomCenter()) {
+			
+		}
+		
+		return location;
 	}
 }
